068b960c3ff3f7611d039dd5cfa81e0fdadc3042
Mateus Mourão <mmdfmateus@gmail.com>
2022-10-13 20:43:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export const App = () => {
+
+  return (
+    <>
+      <h1 className="text text-2xl">Hello World</h1>
+      <button className='bg-cyan-500 p-4 rounded text-white hover:bg-cyan-300'>Enviar</button>
+    </>
+  )
+}
+
