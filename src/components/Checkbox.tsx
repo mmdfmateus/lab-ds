3b3e93c24857eb3ed12da59a5dca67b6d8a615f8
Mateus Mourão <mmdfmateus@gmail.com>
2022-10-15 12:11:58 -0300

    fix props from components

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,12 +1,13 @@
 import * as RadixCheckbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react'
-export interface CheckboxProps {
-}
 
-export const Checkbox = ({ }: CheckboxProps) => {
+export interface CheckboxProps extends RadixCheckbox.CheckboxProps { }
+
+export const Checkbox = (props: CheckboxProps) => {
   return (
     <RadixCheckbox.Root
       className='w-6 h-6 p-{2px} bg-gray-800 rounded flex justify-center items-center'
+      {...props}
     >
       <RadixCheckbox.Indicator asChild>
         <Check weight='bold' className='h-5 w-5 text-cyan-500'/>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,10 +5,11 @@ import { ReactNode } from "react";
 export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode,
-  asChild?: boolean
+  asChild?: boolean,
+  className?: string
 }
 
-export const Text = ({ size = 'md', children, asChild }: TextProps) => {
+export const Text = ({ size = 'md', children, asChild, className }: TextProps) => {
   const Comp = asChild ? Slot : 'span';
   return (
     <Comp 
@@ -18,7 +19,8 @@ export const Text = ({ size = 'md', children, asChild }: TextProps) => {
           'text-xs': size === 'sm',
           'text-sm': size === 'md',
           'text-md': size === 'lg',
-        }
+        },
+        className
       )}
     >
       {children}
